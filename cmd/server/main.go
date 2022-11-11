f4c318569c7c449abc78e05ed624ab2821459f4b
akar <mr.ridwan.thekeeper@gmail.com>
2022-11-11 11:20:56 +0700

    Structuring our application

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,6 +2,18 @@ package main
 
 import "fmt"
 
+type App struct{}
+
+func (app *App) Run() error {
+	fmt.Println("Starting Up Our API")
+	return nil
+}
+
 func main() {
 	fmt.Println("Go REST API Course")
+	app := App{}
+	if err := app.Run(); err != nil {
+		fmt.Println("Error starting up our REST API")
+		fmt.Println(err)
+	}
 }
